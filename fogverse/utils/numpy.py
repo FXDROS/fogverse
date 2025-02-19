578118c6814abd7186483deaf0e516f4638bbb88
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-02-19 22:18:54 +0900

    fix: circular dependencies

--- fogverse/utils/numpy.py
+++ fogverse/utils/numpy.py
@@ -1,8 +1,6 @@
 import numpy as np
 from io import BytesIO
 
-from fogverse.utils.image_process import _encode
-
 def bytes_to_numpy(bbytes):
     f = BytesIO(bbytes)
     return np.load(f, allow_pickle=True)
