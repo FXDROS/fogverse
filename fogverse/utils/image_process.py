cf075356903fc65120a143dfb51fbd6ea18c47ab
FXDROS <57487697+FXDROS@users.noreply.github.com>
2025-02-19 22:15:29 +0900

    fix: circular dependencies

--- fogverse/utils/image_process.py
+++ fogverse/utils/image_process.py
@@ -1,3 +1,4 @@
+import base64
 import cv2
 import uuid
 import os
@@ -10,6 +11,11 @@ def get_cam_id():
 def _encode(img, encoding, *args):
     _, encoded = cv2.imencode(f'.{encoding}', img, *args)
     return encoded
+
+def numpy_to_base64_url(img, encoding, *args):
+    img = _encode(img, encoding, *args)
+    b64 = base64.b64encode(img).decode()
+    return f'data:image/{encoding};base64,{b64}'
 
 def compress_encoding(img, encoding, *args):
     encoded = _encode(img, encoding, *args)
